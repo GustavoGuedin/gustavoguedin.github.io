5b7535c00dc071bf1262e3131685b2f2b6e62760
tiago_muzikante <tiagommuzikante@hotmail.com>
2025-09-04 16:30:02 -0300

    fix quebra/main.js - refactoring using jquery

--- quebra/main.js
+++ quebra/main.js
@@ -1,47 +1,12 @@
-const breakInput = document.getElementById('breakInput');
-const weightResult = document.getElementById('weightResult');
-const metreageResult = document.getElementById('metreageResult');
-const tileSize = document.getElementsByName('tile-size');
-const recipent = document.getElementsByName('recipent');
-let selectedTileSize = document.querySelector('input[name=tile-size]:checked');
-let selectedRecipent = document.querySelector('input[name=recipent]:checked');
+$("input").on('input change', () => {
+	let calculatedWeightResult = (parseFloat($("#breakInput").val()) - parseFloat($("input[name=recipent]:checked").val()));
+	let calculatedMetreageResult = calculatedWeightResult / parseFloat($("input[name=tile-size]:checked").val());
 
-function updateSelectedTileSize() {
-    selectedTileSize = document.querySelector('input[name=tile-size]:checked');
-	mainCalculation();
-}
-
-tileSize.forEach(radio => {
-    radio.addEventListener('change', updateSelectedTileSize);
-})
-
-function updateSelectedRecipent() {
-    selectedRecipent = document.querySelector('input[name=recipent]:checked');
-	mainCalculation();
-}
-
-recipent.forEach(radio => {
-    radio.addEventListener('change', updateSelectedRecipent);
-})
-
-function mainCalculation() {
-	const calculatedWeightResult
-		= (parseFloat(breakInput.value)
-		- parseFloat(selectedRecipent.value));
-	
-	const calculatedMetreageResult
-		= ((parseFloat(breakInput.value)
-		- parseFloat(selectedRecipent.value))
-		/ parseFloat(selectedTileSize.value));
-
-	if (breakInput.value == "") {
-		weightResult.textContent = "0 kg";
-		metreageResult.textContent = "0.00 m²";	
+	if ($("#breakInput").val() == "") {
+		$("#weightResult").text("0 kg");
+		$("#metreageResult").text("0.00 m²");
 	} else {
-		weightResult.textContent = (calculatedWeightResult).toFixed(0) + " kg";
-		metreageResult.textContent = (calculatedMetreageResult).toFixed(2) + " m²";
+		$("#weightResult").text(calculatedWeightResult.toFixed(0) + " kg");
+		$("#metreageResult").text((calculatedMetreageResult).toFixed(2) + " m²");
 	}
-	
-}
-
-breakInput.addEventListener('input', mainCalculation);
+})
